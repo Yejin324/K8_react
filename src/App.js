b8db15fb30829884095b26588c9636a35413fa0b
Yejin324 <cheonyj96@gmail.com>
2024-09-23 15:06:16 +0900

    REACT240923

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+// import Hello from '/01/Hello';
+import { IoHomeSharp } from "react-icons/io5";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="w-full h-screen
+                    flex flex-col justify-center items-center">
+      <header className="w-full h-20
+                        flex justify-between items-center 
+                        bg-slate-200">
+      <div>
+      <p>리액트 기초</p>  
+      <p>K8기 디지털 트레이닝</p>
+      </div> 
+      <div className="w-10">
+        <IoHomeSharp className="w-10"/>
+      </div>               
+      </header> 
+      <main className="w-full grow
+                      flex flex-col justify-center items-center
+                      overflow-y-scroll">
+
+      </main>
+      <footer className="w-full h-20
+                        flex justify-center items-center
+                        bg-black text-white">
+      안녕                   
+      </footer>
     </div>
   );
 }
